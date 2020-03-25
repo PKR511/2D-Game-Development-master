6701245e4bcfa56182e6a5f85e0213d9032e81c5
PKR511 <57657443+PKR511@users.noreply.github.com>
2020-03-25 13:40:44 +0530

    Added Animation Idel,Walk,Run,Jump

---
+++ Assets/Scripts/Helper/MyTags.cs
@@ -0,0 +1,18 @@
+﻿
+public class MyTags  {
+
+	//Animations Tags
+	public static string PLAYER_WALK_ANIM 	= "walk";
+	public static string PLAYER_RUN_ANIM 	= "run";
+	public static string PLAYER_JUMP_ANIM 	= "jum";
+
+	//Layers Tag
+	public static string GROUND_LAYER   = "Ground";
+	public static string PLAYER_LAYER   = "Ground";
+
+	//Tags Tag
+	public static string GROUND_TAG   = "Ground";
+	public static string PLAYER_TAG   = "Ground";
+
+
+}//Class
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//public variables
+	public float walkSpeed = 5f;
+	public float jumpPower = 12f;
+	public float runSpeed= 5f;
+
+	public Transform groundCheckPosition;
+	public LayerMask groundLayer;
+
+	//private variables
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	private bool isGrounded;
+	private bool jumped;
+
+
+
+	void Awake() {
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		CheckIfGrounded ();
+		PlayerJump ();
+	}
+
+	void FixedUpdate() {
+		PlayerWalk ();
+	}
+
+	void PlayerWalk() {
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		string playanim;
+		float speed;
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			playanim = MyTags.PLAYER_RUN_ANIM;
+			speed = runSpeed;
+		} else {
+			playanim = MyTags.PLAYER_WALK_ANIM;
+			speed = walkSpeed;
+		}
+
+		if (h > 0) {
+			myBody.velocity = new Vector2 (speed, myBody.velocity.y);
+
+			ChangeDirection (1);
+
+		} else if (h < 0) {
+			myBody.velocity = new Vector2 (-speed, myBody.velocity.y);
+
+			ChangeDirection (-1);
+
+		} else {
+			myBody.velocity = new Vector2 (0f, myBody.velocity.y);
+		}
+
+
+		anim.SetInteger (playanim, Mathf.Abs((int)myBody.velocity.x));
+
+	}
+
+	void ChangeDirection(int direction) {
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = direction;
+		transform.localScale = tempScale;
+	}
+
+	void CheckIfGrounded() {
+		isGrounded = Physics2D.Raycast (groundCheckPosition.position, Vector2.down, 0.1f, groundLayer);
+
+		if (isGrounded) {
+			// and we jumped before
+			if (jumped) {
+
+				jumped = false;
+
+				anim.SetBool (MyTags.PLAYER_JUMP_ANIM , false);
+			}
+		}
+
+	}
+
+	void PlayerJump() {
+		if (isGrounded) {
+			if (Input.GetKey (KeyCode.Space)) {
+				jumped = true;
+				myBody.velocity = new Vector2 (myBody.velocity.x, jumpPower);
+
+				anim.SetBool (MyTags.PLAYER_JUMP_ANIM, true);
+			}
+		}
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
