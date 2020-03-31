e327b327468496d40c7eaaaf09b72e08a9a0a91a
PKR511 <57657443+PKR511@users.noreply.github.com>
2020-03-31 18:28:41 +0530

    First Level
    First Level Playable  but haven`t added Game Over Panel.

---
+++ Assets/Scripts/Enemy/Chomper.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chomper : MonoBehaviour
+{
+	public float moveSpeed = 1f;
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	public LayerMask playerLayer;
+	public LayerMask groundLayer;
+	private bool moveLeft;
+
+	private bool canMove;
+	private bool stunned;
+
+	public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+	private Vector3 left_Collision_Pos, right_Collision_Pos;
+
+	private AudioSource audioManager;
+
+	void Awake() {
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+
+		left_Collision_Pos = left_Collision.position;
+		right_Collision_Pos = right_Collision.position;
+		audioManager = GetComponent<AudioSource> ();
+	}
+
+	void Start () {
+		moveLeft = true;
+		canMove = true;
+	}
+
+	void Update () {
+		if (canMove) {
+			if (moveLeft) {
+				myBody.velocity = new Vector2 (-moveSpeed, myBody.velocity.y);
+			} else {
+				myBody.velocity = new Vector2 (moveSpeed, myBody.velocity.y);
+			}
+		}
+
+		CheckCollision ();
+
+	}
+
+	void CheckCollision() {
+
+		RaycastHit2D leftHit = Physics2D.Raycast (left_Collision.position, Vector2.left, 0.1f, playerLayer);
+		RaycastHit2D rightHit = Physics2D.Raycast (right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+		Collider2D topHit = Physics2D.OverlapCircle (top_Collision.position, 0.1f, playerLayer);
+
+		if (topHit != null) {
+			if (topHit.gameObject.tag == MyTags.PLAYER_TAG) {
+				if (!stunned) {
+					topHit.gameObject.GetComponent<Rigidbody2D> ().velocity =
+						new Vector2 (topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+
+					canMove = false;
+					myBody.velocity = new Vector2 (0, 0);
+
+					anim.Play (MyTags.ENEMY_IDLE_ANIM);
+					stunned = true;
+
+
+					if(tag == MyTags.SPITTER_TAG) {
+						//anim.Play (MyTags.ENEMY_IDLE_ANIM);
+						StartCoroutine (Dead (0.5f));
+					}
+				}
+			}
+		}
+
+		if (leftHit) {
+			if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
+				if (!stunned) {
+					// APPLY DAMAGE TO PLAYER
+					leftHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
+				} else {
+											
+						StartCoroutine (Dead (3f));
+
+				}
+			}
+		}
+
+		if (rightHit) {
+			if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
+				if (!stunned) {
+					// APPLY DAMAGE TO PLAYER
+					rightHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
+				} else {
+					
+						
+						StartCoroutine (Dead (3f));
+
+				}
+			}
+		}
+
+
+		if (!Physics2D.Raycast (down_Collision.position, Vector2.down, 0.1f,groundLayer)) {
+
+			ChangeDirection ();
+		}
+
+	}
+
+	void ChangeDirection() {
+
+		moveLeft = !moveLeft;
+
+		Vector3 tempScale = transform.localScale;
+
+		if (moveLeft) {
+			tempScale.x = -Mathf.Abs (tempScale.x);
+
+			//left_Collision.position = left_Collision_Pos;
+			//right_Collision.position = right_Collision_Pos;
+
+		} else {
+			tempScale.x = Mathf.Abs (tempScale.x);
+
+			//right_Collision.position = left_Collision_Pos;
+			//left_Collision.position = right_Collision_Pos;
+
+		}
+
+		transform.localScale = tempScale;
+
+	}
+
+	IEnumerator Dead(float timer) {
+		anim.Play (MyTags.ENEMY_DEAD_ANIM);
+		audioManager.Play ();
+		yield return new WaitForSeconds (timer);
+		gameObject.SetActive (false);
+	}
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/Scripts/Helper/MyTags.cs
+++ Assets/Scripts/Helper/MyTags.cs
@@ -1,18 +1,35 @@
 ﻿
 public class MyTags  {
 
-	//Animations Tags
-	public static string PLAYER_WALK_ANIM 	= "walk";
-	public static string PLAYER_RUN_ANIM 	= "run";
-	public static string PLAYER_JUMP_ANIM 	= "jum";
+	//----------------------------------------------Animations Tags Start----------------------------------------
+	//Player Animations param
+	public static string PLAYER_IDLE_PARAM 	= "idle";
+	public static string PLAYER_WALK_PARAM 	= "walk";
+	public static string PLAYER_RUN_PARAM 	= "run";
+	public static string PLAYER_JUMP_PARAM	= "jump";
 
-	//Layers Tag
+	//------------------------------------------Enemy Animations Start--------------------------------------------
+	//Chomper And Spitter Animation 
+	public static string ENEMY_IDLE_ANIM 	= "Idle";
+	public static string ENEMY_WALK_ANIM 	= "Walk";
+	public static string ENEMY_DEAD_ANIM 	= "Death";
+
+	//------------------------------------------Enemy Animations End--------------------------------------------
+
+
+	//----------------------------------------------Animations Tags End----------------------------------------
+
+	//--------------------------------------------Layers Tag Start----------------------------------------------
 	public static string GROUND_LAYER   = "Ground";
-	public static string PLAYER_LAYER   = "Ground";
-
-	//Tags Tag
+	public static string PLAYER_LAYER   = "Player";
+	public static string ENEMY_LAYER    = "Enemy";
+	//--------------------------------------------Layers Tag End----------------------------------------------
+	//--------------------------------------------Tags Tag Start------------------------------------------------
 	public static string GROUND_TAG   = "Ground";
-	public static string PLAYER_TAG   = "Ground";
+	public static string PLAYER_TAG   = "Player";
+	public static string CHOMPER_TAG   = "Chomper";
+	public static string SPITTER_TAG   = "Spitter";
+	//--------------------------------------------Tags Tag Start------------------------------------------------
 
 
 }//Class
